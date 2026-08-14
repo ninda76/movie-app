8b74af2c0656de0f87c69beee5f8420f329235df
ninda76 <nuridareborn@gmail.com>
2026-08-14 16:19:13 +0700

    Fix HTTPS proxy for Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-   ->withMiddleware(function (Middleware $middleware) {
-    $middleware->web(append: [
-        \App\Http\Middleware\HandleInertiaRequests::class,
-    ]);
+ ->withMiddleware(function (Middleware $middleware): void {
+    $middleware->trustProxies(at: '*');
 })
 
     ->withExceptions(function (Exceptions $exceptions): void {
